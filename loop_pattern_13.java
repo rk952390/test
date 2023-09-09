15c0cfe5a8d68bf4e56e3b873f9c88b13f199f6a
Rahul Kumar <142075106+rk952390@users.noreply.github.com>
2023-09-09 09:15:30 +0530

    Add files via upload

---
+++ loop_pattern_13.java
@@ -0,0 +1,23 @@
+// * * *  here rows =6 and column =3
+// * * *
+// * * *
+// * * *
+// * * *
+// * * *
+
+public class loop_pattern_13 {
+    public static void main(String args[])
+    {
+        for(int i=1; i<=6; i++)
+        {
+            for(int j = 1; j<=3; j++)
+            {
+                System.out.print(" * ");
+                //System.out.print(" R ");
+
+            }
+            System.out.println();
+        }
+
+    }
+}
