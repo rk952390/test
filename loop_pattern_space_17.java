1d4420b54e4be34cd22774f8bb7d34d98f567910
Rahul Kumar <142075106+rk952390@users.noreply.github.com>
2023-09-09 11:25:10 +0530

    Add files via upload

---
+++ loop_pattern_space_17.java
@@ -0,0 +1,33 @@
+//        *
+//      * *
+//    * * *
+//  * * * *
+//* * * * *
+
+
+import java.util.Scanner;
+public class loop_pattern_space_17 {
+    public static void main(String args[])
+    {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("enter the number=");
+            int n = sc.nextInt();
+            // outer loop
+            for(int i=1; i<=n; i++)
+            {
+                //loop for space
+                for(int j=1;j<=n-i;j++)
+                {
+                    System.out.print(" ");
+                }
+                for(int k=1;k<=i;k++)
+                {
+                    System.out.print("*");
+
+                }
+                System.out.println();
+            }
+        }
+
+    }
+}
