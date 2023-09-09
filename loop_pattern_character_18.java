784d999b89c3965d8d816bff6c272e4f8902fd9f
Rahul Kumar <142075106+rk952390@users.noreply.github.com>
2023-09-09 11:25:40 +0530

    Add files via upload

---
+++ loop_pattern_character_18.java
@@ -0,0 +1,26 @@
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+
+
+
+
+import java.util.Scanner;
+public class loop_pattern_character_18 {
+    public static void main(String args[])
+    {
+        try (Scanner sc = new Scanner (System.in)) {
+            System.out.print("enter the number=");
+            int n = sc.nextInt();
+            for(int i = 1; i<=n; i++)
+            {
+                for(int j=1; j<=i;j++)
+                {
+                    
+                }
+            }
+        }
+
+    }
+}
